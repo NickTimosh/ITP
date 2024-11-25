36687012767e4279200ca231cfebe65c0dd034ec
Nikita Tymoshenko <116592259+NickTimosh@users.noreply.github.com>
2024-11-25 09:51:27 -0500

    viz

--- musicVis/spectrum.js
+++ musicVis/spectrum.js
@@ -6,11 +6,25 @@ function Spectrum(){
 		var spectrum = fourier.analyze();
 		noStroke();
 		
-		fill(0,255,0)
 		for (var i = 0; i< spectrum.length; i++){
-			var x = map(i, 0, spectrum.length, 0, width);
-		    var h = -height + map(spectrum[i], 0, 255, height, 0);
-		    rect(x, height, width / spectrum.length, h );
+
+			// mapping the indix to the vertical position
+			var y = map(i, 0, spectrum.length, 0, height);
+
+			// maping the amp to the width of the bar
+			var w = map(spectrum[i], 0, 255, 0, width)
+
+				// determine the color based on the amp
+			var r = spectrum[i];
+			var g = 255 - spectrum[i] // inverse of red
+			var b = 0
+
+			fill(r,g,b);
+			rect(0, y, w, height/spectrum.length)
+
+			// var x = map(i, 0, spectrum.length, 0, width);
+		    // var h = -height + map(spectrum[i], 0, 255, height, 0);
+		    // rect(x, height, width / spectrum.length, h );
   		}
 	
 		pop();
